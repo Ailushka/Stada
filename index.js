256584ac64d23750dc33a9e7ee6d0b8c8745dc3c
Yulia Okhotina <y.chaser@yandex.ru>
2023-05-31 01:29:14 +0300

    Added price section with tabs

--- index.js
+++ index.js
@@ -221,3 +221,76 @@ document.querySelectorAll('.accordeon__button').forEach((item) => {
     }
   })
 })
+
+/* -------------------- */
+/*         Tabs         */
+/* -------------------- */
+
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = document.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', changeTabFocus);
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel);
+});
+
+
+let tabFocus = 0;
+function changeTabFocus(evt) {
+    const keydownLeft = 37;
+    const keydownRight = 39;
+
+    if (evt.keyCode === keydownLeft || evt.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+    }
+
+    if (evt.keyCode === keydownRight) {
+        tabFocus++;
+        if (tabFocus >= tabs.length) {
+            tabFocus = 0;
+        }
+    }
+
+    if (evt.keyCode === keydownLeft) {
+        tabFocus--;
+        if (tabFocus < 0) {
+            tabFocus = tabs.length - 1;
+        }
+    }
+
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
+}
+
+
+function changeTabPanel(evt) {
+    const targetTab = evt.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+
+    const tabContainer = targetTab.parentNode.parentNode;
+    const mainContainer = tabContainer.parentNode;
+
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
+
+    targetTab.setAttribute("aria-selected", true);
+
+    hideContent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, [`#${targetPanel}`]);
+}
+
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => {
+          item.setAttribute("hidden", true);
+          item.classList.remove('tabpanel_active');
+        });
+}
+
+function showContent(parent, content) {
+     parent.querySelector(content).removeAttribute('hidden');
+     parent.querySelector(content).classList.add('tabpanel_active');
+}
