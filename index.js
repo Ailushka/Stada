ec2e86b3dd5e06d9fd668834870afe443792839a
Yulia Okhotina <y.chaser@yandex.ru>
2023-08-17 14:37:37 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,298 +1,4 @@
 /* -------------------- */
-/*     Smooth links     */
-/* -------------------- */
-
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
-
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
-};
-
-/* -------------------- */
-/*      Mobile menu     */
-/* -------------------- */
-
-const burgerButton = document.querySelector('.burger');
-const nav = document.querySelector('.nav');
-const menuLinks = document.querySelectorAll('.nav-list__link');
-
-burgerButton.addEventListener("click", function () {
-    burgerButton.classList.toggle("burger_active");
-    nav.classList.toggle("nav_opened");
-    nav.classList.toggle("transition");
-    document.querySelector('.page').classList.toggle('no-scroll');
-    document.querySelector('html').classList.toggle('no-scroll');
-});
-
-menuLinks.forEach(menuLink => {
-  menuLink.addEventListener('click', () => {
-    burgerButton.classList.remove("burger_active");
-    nav.classList.remove("nav_opened");
-    nav.classList.remove("transition");
-    document.querySelector('.page').classList.remove('no-scroll');
-    document.querySelector('html').classList.remove('no-scroll');
-  })
-});
-
-/* -------------------- */
-/*   Heading-subtitle   */
-/* -------------------- */
-
-const siteSubtitleElement = document.querySelector('.site-subtitle');
-
-const siteSubtitles = [
-  '&rarr; программирование',
-  '&rarr; аналитика и Data Science',
-  '&rarr; менеджмент',
-  '&rarr; маркетинг',
-  '&rarr; дизайн',
-  '&rarr; HR и рекрутмент'
-];
-
-let currentSubtitleIndex = 0;
-
-function renderCurrentSubtitle() {
-  siteSubtitleElement.innerHTML = siteSubtitles[currentSubtitleIndex];
-  siteSubtitleElement.classList.add('offer__site-subtitle_active');
-
-  setTimeout(() => {
-    siteSubtitleElement.classList.remove('offer__site-subtitle_active');
-  }, 1400);
-  currentSubtitleIndex = (currentSubtitleIndex + 1) % siteSubtitles.length;
-}
-
-renderCurrentSubtitle(); // Показываем первый элемент сразу
-
-setInterval(() => {
-  renderCurrentSubtitle();
-}, 2000);
-
-
-/* -------------------- */
-/*         Popup        */
-/* -------------------- */
-
-const requestButtons = document.querySelectorAll('.button_type_request');
-const closeButtons = document.querySelectorAll('.button_type_close');
-const requestPopup = document.querySelector('.popup_type_request');
-const requestForm = document.querySelector('.form_type_request');
-const successPopup = document.querySelector('.popup_type_success');
-const reviewPopup = document.querySelector('.popup_type_review');
-const reviewName = reviewPopup.querySelector('.reviews__name');
-const reviewContent = reviewPopup.querySelector('.reviews__text');
-const reviewImageName = reviewPopup.querySelector('.reviews__image');
-const reviewImageLink = reviewPopup.querySelector('.reviews__image');
-const reviews = document.querySelectorAll('.reviews-list__item');
-const ESCAPE = 27;
-
-function openPopUp(popup) {
-  popup.classList.add('popup_opened');
-  popup.classList.add('transition');
-  document.querySelector('.page').classList.add('no-scroll');
-  document.addEventListener('click', closePopUpByOverlay);
-  document.addEventListener('keydown', closePopUpByEsc);
-
-}
-
-function closePopUp(popup) {
-  popup.classList.remove('popup_opened');
-  popup.classList.remove('transition');
-  document.querySelector('.page').classList.remove('no-scroll');
-  document.removeEventListener('click', closePopUpByOverlay);
-  document.removeEventListener('keydown', closePopUpByEsc);
-}
-
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
-
-function closePopUpByOverlay(evt) {
-  if (evt.target.classList.contains('popup_opened')) {
-    closePopUp(evt.target);
-  }
-}
-
-function closePopUpByEsc(evt) {
-  const popupOpened = document.querySelector('.popup_opened');
-  if (evt.keyCode === ESCAPE) {
-    closePopUp(popupOpened);
-  }
-}
-
-requestButtons.forEach((item) => {
-  item.addEventListener('click', () => {
-      openPopUp(requestPopup);
-  });
-});
-
-reviews.forEach((item) => {
-  item.addEventListener('click', (evt) => {
-    const reviewToOpen = evt.target.closest('.reviews-list__item');
-    const openedReviewName = reviewToOpen.querySelector('.reviews__name').textContent;
-    const openedReviewContent = reviewToOpen.querySelector('.reviews__text').textContent;
-    const openedReviewImageName = reviewToOpen.querySelector('.reviews__image').alt;
-    const openedReviewImageLink = reviewToOpen.querySelector('.reviews__image').src;
-
-    reviewName.textContent = openedReviewName;
-    reviewContent.textContent = openedReviewContent;
-    reviewImageName.alt = openedReviewImageName;
-    reviewImageLink.src = openedReviewImageLink;
-
-      openPopUp(reviewPopup);
-  });
-});
-
-closeButtons.forEach((item) => {
-  item.addEventListener('click', (evt) => {
-      const popUpToClose = evt.target.closest('.popup');
-      closePopUp(popUpToClose);
-  });
-});
-
-/* -------------------- */
-/*       Accordeon      */
-/* -------------------- */
-
-document.querySelectorAll('.accordeon__button').forEach((item) => {
-  item.addEventListener('click', (evt) => {
-    const content = item.querySelector('.accordeon__content');
-
-    if (!item.classList.contains('accordeon__button_active'))
-    {
-      item.classList.add('accordeon__button_active');
-      content.style.maxHeight = content.scrollHeight + 'px';
-    } else {
-      item.classList.remove('accordeon__button_active');
-      content.style.maxHeight = '';
-    }
-  })
-})
-
-/* -------------------- */
-/*   Show-more button   */
-/* -------------------- */
-
-const showMoreButtons = document.querySelectorAll('.button_type_show-more');
-
-showMoreButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const moreContent = button.closest('.expert').querySelector('.expert__extra-description');
-
-    if (button.getAttribute('aria-expanded') === 'false') {
-      button.setAttribute('aria-expanded', 'true');
-      moreContent.style.maxHeight = moreContent.scrollHeight + 'px';
-    } else {
-      button.setAttribute('aria-expanded', 'false');
-      moreContent.style.maxHeight = '';
-    }
-  })
-})
-
-/* -------------------- */
-/*    Carousel slider   */
-/* -------------------- */
-
-const sliderInstances = {};
-
-const initSlider = (slider) => {
-  const sliderContainer = slider.querySelector('.slider__container');
-  const sliderItems = slider.querySelectorAll('.slider__item');
-  const next = slider.querySelector('.slide-arrow_next');
-  const prev = slider.querySelector('.slide-arrow_prev');
-  const slideGap = parseInt(getComputedStyle(sliderContainer).gap, 10);
-  let currentSlide = 0;
-
-  sliderItems[currentSlide].classList.add("activeSlide");
-
-  let itemWidth = sliderItems[0].clientWidth;
-
-  const sliderItemsToShow = Math.floor(sliderContainer.clientWidth / itemWidth);
-
-  let resizeTimer;
-
-  function handleResize() {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function () {
-      currentSlide = 0;
-      sliderContainer.style.transform = "translateY(0px)";
-      prev.classList.add("slide-arrow_disabled");
-      next.classList.remove("slide-arrow_disabled");
-      itemWidth = sliderItems[0].clientWidth;
-    }, 200);
-  }
-
-  sliderContainer.style.transform = "translateY(0px)";
-
-  const toggleClass = (condition, elem, className) => {
-    condition
-      ? elem.classList.add(className)
-      : elem.classList.remove(className);
-  };
-
-  const handlePrevNextBtn = () => {
-    toggleClass(currentSlide === 0, prev, "slide-arrow_disabled");
-    toggleClass(currentSlide === sliderItems.length - sliderItemsToShow, next, "slide-arrow_disabled");
-  };
-
-  handlePrevNextBtn();
-
-  const handleSlide = (condition, slideElem, event) => {
-    const slideTransformValue = slideElem.style.transform;
-    const translateXValue = slideTransformValue.replace(/[^\d.]/g, "");
-
-    if (condition && event === "next") {
-      currentSlide += 1;
-      slideElem.style.transform = `translateX(-${
-        +translateXValue + itemWidth + slideGap
-      }px)`;
-    } else if (condition && event === "prev") {
-      currentSlide -= 1;
-      slideElem.style.transform = `translateX(-${
-        +translateXValue - itemWidth - slideGap
-      }px)`;
-    }
-    sliderItems.forEach((e) => e.classList.remove("activeSlide"));
-    sliderItems[currentSlide].classList.add("activeSlide");
-  };
-
-  const handleNextClick = () => {
-    handleSlide(currentSlide !== sliderItems.length - sliderItemsToShow, sliderContainer, "next");
-    handlePrevNextBtn();
-  };
-
-  const handlePrevClick = () => {
-    handleSlide(currentSlide !== 0, sliderContainer, "prev");
-    handlePrevNextBtn();
-  };
-
-  next.addEventListener("click", handleNextClick);
-  prev.addEventListener("click", handlePrevClick);
-
-  const removeClickHandlers = () => {
-    next.removeEventListener("click", handleNextClick);
-    prev.removeEventListener("click", handlePrevClick);
-  };
-
-  sliderInstances[slider.getAttribute("id")] = {
-    removeClickHandlers,
-    sliderContainer,
-    next,
-    prev
-  };
-};
-
-const sliders = document.querySelectorAll('.slider');
-
-sliders.forEach(slider => initSlider(slider));
-
-/* -------------------- */
 /*         Tabs         */
 /* -------------------- */
 
@@ -302,78 +8,65 @@ const tabs = document.querySelectorAll('[role="tab"]');
 tabList.addEventListener('keydown', changeTabFocus);
 
 tabs.forEach((tab) => {
-  tab.addEventListener('click', changeTabPanel);
+    tab.addEventListener('click', changeTabPanel);
 });
 
+
 let tabFocus = 0;
-
 function changeTabFocus(evt) {
-  const keydownLeft = 37;
-  const keydownRight = 39;
+    const keydownLeft = 37;
+    const keydownRight = 39;
 
-  if (evt.keyCode === keydownLeft || evt.keyCode === keydownRight) {
-    tabs[tabFocus].setAttribute("tabindex", -1);
-  }
-
-  if (evt.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0;
+    if (evt.keyCode === keydownLeft || evt.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
     }
-  }
 
-  if (evt.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1;
+    if (evt.keyCode === keydownRight) {
+        tabFocus++;
+        if (tabFocus >= tabs.length) {
+            tabFocus = 0;
+        }
     }
-  }
 
-  tabs[tabFocus].setAttribute("tabindex", 0);
-  tabs[tabFocus].focus();
+    if (evt.keyCode === keydownLeft) {
+        tabFocus--;
+        if (tabFocus < 0) {
+            tabFocus = tabs.length - 1;
+        }
+    }
+
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
 }
 
+
 function changeTabPanel(evt) {
-  const targetTab = evt.target;
-  const targetPanel = targetTab.getAttribute("aria-controls");
+    const targetTab = evt.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
 
-  const tabContainer = targetTab.parentNode.parentNode;
-  const mainContainer = tabContainer.parentNode;
+    const tabContainer = targetTab.parentNode.parentNode;
+    const mainContainer = tabContainer.parentNode;
 
-  tabContainer
-    .querySelector('[aria-selected="true"]')
-    .setAttribute("aria-selected", false);
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
 
-  targetTab.setAttribute("aria-selected", true);
+    targetTab.setAttribute("aria-selected", true);
 
-  hideContent(mainContainer, '[role="tabpanel"]');
-  showContent(mainContainer, [`#${targetPanel}`]);
+    hideContent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, [`#${targetPanel}`]);
 }
 
 function hideContent(parent, content) {
-  parent
-    .querySelectorAll(content)
-    .forEach((item) => {
-      item.setAttribute("hidden", true);
-      item.classList.remove('tabpanel_active');
-      const panelId = item.getAttribute("id");
-      const sliderInstance = sliderInstances[panelId];
-      if (sliderInstance) {
-        sliderInstance.removeClickHandlers();
-        sliderInstance.sliderContainer.classList.remove('slider');
-      }
-    });
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => {
+          item.setAttribute("hidden", true);
+          item.classList.remove('tabpanel_active');
+        });
 }
 
 function showContent(parent, content) {
-  parent.querySelector(content).removeAttribute('hidden');
-  parent.querySelector(content).classList.add('tabpanel_active');
-
-  const panelId = parent.querySelector(content).getAttribute("id");
-  const sliderInstance = sliderInstances[panelId];
-  if (sliderInstance && parent.classList.contains('process__content')) {
-    parent.querySelector(content).classList.add('slider');
-    sliderInstance.removeClickHandlers();
-    initSlider(parent.querySelector(content));
-  }
+     parent.querySelector(content).removeAttribute('hidden');
+     parent.querySelector(content).classList.add('tabpanel_active');
 }
